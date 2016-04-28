261c711a898563e486f109512b2f31fffb8af651
AmitaiB <blickstein@gmail.com>
2016-04-28 17:01:52 -0400

    Adds CommonRss (currently has color scheme colors)

---
+++ DoITT_BookSelector/CommonRss.swift
@@ -0,0 +1,20 @@
+//
+//  CommonRss.swift
+//  DoITT_BookSelector
+//
+//  Created by Amitai Blickstein on 4/28/16.
+//  Copyright © 2016 Amitai Blickstein, LLC. All rights reserved.
+//
+
+import UIKit
+
+// colors
+
+ /// For active backgrounds (in switches, scrollviews, etc.)
+internal let allportsBlueColor = UIColor(red:0.00, green:0.45, blue:0.68, alpha:1.00)
+
+/// For NavigationBar, menus, and other Things That Don't Move
+internal let onyxBlackColor = UIColor(red:0.06, green:0.06, blue:0.06, alpha:1.00)
+
+ /// White, the background color that makes everything else shine.
+internal let romanceWhiteColor = UIColor(red:1.0, green:1.0, blue:1.0, alpha:1.00)
