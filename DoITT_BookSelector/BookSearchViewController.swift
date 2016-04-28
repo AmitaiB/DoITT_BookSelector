06b822a1d9d582b20bce67d192033bee7ef7e033
AmitaiB <blickstein@gmail.com>
2016-04-28 18:20:57 -0400

    Refactor pt1: preps single View Controller with SearchBar to assume responsibility for the whole app

--- DoITT_BookSelector/BookSearchViewController.swift
+++ DoITT_BookSelector/BookSearchViewController.swift
@@ -8,29 +8,21 @@
 
 import UIKit
 
+internal let kSearchResultsCellReuseID = "searchResultsCellReuseID"
+
 class BookSearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var resultsTableView: UITableView!
+    var searchResults = [Book]()
+    
+    
+    
     @IBOutlet weak var titleDisplayLabel: UILabel!
     @IBOutlet weak var authorDisplayLabel: UILabel!
     @IBOutlet weak var descriptionDisplayLabel: UILabel!
     
-    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
-        if sender === searchButton && searchField.text!.isEmpty {
-            return false
-        } else {
-            return true
-        }
-    }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "BookSelectionSegueID" {
-            let bookSelectionVC = segue.destinationViewController as! BookSelectionViewController
-            bookSelectionVC.searchString = searchField.text
-        }
-    }
-
     @IBAction func displaySelectionInMainViewController(segue: UIStoryboardSegue) {
         let bookSelectionVC = segue.sourceViewController as! BookSelectionViewController
         titleDisplayLabel.text = bookSelectionVC.bookSelection?.title
@@ -39,7 +31,74 @@ class BookSearchViewController: UIViewController {
     }
 }
 
-extension BookSearchViewController: UISearchBarDelegate {
+// MARK: - UITableViewDataSource
+
+extension BookSearchViewController: UITableViewDataSource {
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kSearchResultsCellReuseID, forIndexPath: indexPath)
+        
+        let thisBook = searchResults[indexPath.row]
+        
+        cell.textLabel?.text = thisBook.title
+        cell.detailTextLabel?.text = thisBook.author
+        
+        return cell
+    }
 }
 
+
+// MARK: - UITableViewDelegate
+
+extension BookSearchViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+        
+        let thisBook = searchResults[indexPath.row]
+        
+        /// TODO: Send thisBook's data to be displayed here
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension BookSearchViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if !searchText.isEmpty {
+            resultsTableView.hidden = false
+            
+            /// TODO: Network call for search query string -> update dataSource obj in completion, reloadData
+        }
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+        resultsTableView.hidden = true
+    }
+    
+    // MARK: - Helpers
+    
+    func handleSearchError(error: NSError) {
+        let alert = UIAlertController(title: "Error",
+                                      message: error.localizedDescription,
+                                      preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Cancel,
+                                         handler: nil)
+        alert.addAction(cancelAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
--- DoITT_BookSelector/BookSelectionViewController.swift
+++ DoITT_BookSelector/BookSelectionViewController.swift
@@ -7,9 +7,6 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftyJSON
-import AlamofireSwiftyJSON
 
 class BookSelectionViewController: UIViewController {
 
